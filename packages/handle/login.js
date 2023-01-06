03ccda0a6e8400d44cac5e8bb0ad3506ebb4748e
liuzx <liuzxdyx@gmail.com>
2023-01-06 21:27:10 +0800

    feat: handle/login

--- packages/handle/login.js
+++ packages/handle/login.js
@@ -1,5 +1,24 @@
+const zhixue = require("../model/request");
 exports.apply = function (ctx, utils) {
-	utils.express.app.get('/hello', (req, res) => {
-		res.send('hi');
+	utils.express.app.post('/login', async(req, res) => {
+		/*
+		Request Parameters:
+			username: string 		//username for Zhixue.net
+			password: string
+		Response Data:
+            token: string;
+            childId: string;
+            user: {
+				name: string;
+				userid: string;
+            };
+            class: {
+                name: string;
+            };
+            school: {
+                name: string;
+			};
+		*/
+		res.send(await zhixue.login(req.body.username, req.body.password));
 	});
 };
--- utils/express.js
+++ utils/express.js
@@ -3,7 +3,9 @@ const express = require('express');
 exports.app = new express();
 
 exports.init = function (utils) {
+	utils.express.app.use(express.json());
+	utils.express.app.use(express.urlencoded());
 	utils.express.app.listen(3000, () => {
-		
+
 	});
 };
